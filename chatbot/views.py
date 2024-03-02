0b09467bac0473536f8b0779721d59d1091a6b28
ritik <ritikbhatt020@gmail.com>
2024-03-02 18:56:45 +0530

    ⚓Basic fn for registration

--- chatbot/views.py
+++ chatbot/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import openai
 
+from django.contrib.auth import logout, login, authenticate
+
 # Create your views here.
 
-openai_api_key = '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'
+openai_api_key = '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'
 openai.api_key = openai_api_key
 
 def ask_openai(message):
@@ -25,3 +27,23 @@ def chatbot(request):
         response = ask_openai(message)
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        
+        if password1 == password2:
+            pass
+        else:
+            error_message = "Passwords don't match"
+            return render(request, 'register.html', {'error_message': error_message})
+    return render(request, 'register.html')
+
+def logout(request):
+    return logout(request)
