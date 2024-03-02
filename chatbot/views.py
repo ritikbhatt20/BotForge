34813d124c2a156275a7aaf78bc8387d5ccfd971
ritik <ritikbhatt020@gmail.com>
2024-03-02 17:34:47 +0530

    🏹Integrated gpt-3.5 openai api

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import openai
 
 # Create your views here.
 
+openai_api_key = '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'
+openai.api_key = openai_api_key
+
+def ask_openai(message):
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo-0125",
+        messages=[
+            {"role": "system", "content": "You are an helpful assistant."},
+            {"role": "user", "content": message},
+        ]
+    )
+    print(response)
+    answer = response.choices[0].message.content.strip()
+    return answer
+
 def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = ask_openai(message)
+        return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
